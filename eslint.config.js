c84bb4a7eda2898a01d566e1175fd4389d69bdc1
Yash Lahase <yashlahase1234@gmail.com>
2026-02-23 17:54:10 +0530

    update github actions

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,13 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+      },
     },
     rules: {
       "no-unused-vars": "error",
